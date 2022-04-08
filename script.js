7f818b7d09a51d6df8ba69fd450e1cf6fea78784
Eugenia Pastore <eugenia.pastore@gmail.com>
2022-04-08 20:29:11 +0200

    function can be defined with or without parameters, function can produce/return the value, in this case the result has to be stored in a variable.

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ const passTest = true;
 
 if (passTest) hasDrivingLicense = true;
 if (hasDrivingLicense) console.log('Can drive');
+
+//calling=running=evoking=executing=using function that we defined previously
+// this function does not return any value
+function logger() {
+	console.log("My name is Eugenia");
+}
+logger();
+// functions can receive and return data
+
+function fruitProcessor(apples, oranges) {
+	const juice = `Juice with ${apples} apples and ${oranges} oranges`
+	return juice;
+}
+//need to store in a variable in order to use juice value
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
